5e9845afc8610ff03265413044e2a9c7c3644013
sceonn <yoou201@naver.com>
2024-10-10 13:12:02 +0900

    helmet

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -11,6 +11,7 @@ import GlobalStyles from "components/GlobalStyles"
 import Header from "./Header"
 import Body from "./Body"
 import Footer from "./Footer"
+import { Helmet } from "react-helmet"
 
 const Layout = ({ children }) => {
   const dispatch = useDispatch()
@@ -42,6 +43,9 @@ const Layout = ({ children }) => {
     <ThemeProvider theme={theme === "light" ? light : dark}>
       <GlobalStyles />
       <Header toggleTheme={toggleTheme} />
+      <Helmet>
+        <meta name="google-site-verification" content="-VotWFcrrhgItHK-Bskxa9cCUaCfREvBjQ-ZsSZRRp8" />
+      </Helmet>
       <Body>{children}</Body>
       <Footer />
     </ThemeProvider>
